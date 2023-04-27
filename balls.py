09304002ac40e8e0a0611041e7567ec89f12ec96
Joshua Pilcher <131650826+joshpilcher@users.noreply.github.com>
2023-04-27 12:08:45 +1000

    Update balls.py

--- balls.py
+++ balls.py
@@ -1,18 +1,19 @@
-import csv
-
-str_category = "Category"
-unique_list = []
-category_count = {}
-file_infile = open("Google Play Store.csv", "r")
-csv_dict_reader = csv.DictReader(file_infile)
-
-for row in csv_dict_reader:
-    category = row["Category"]
-    if category not in unique_list:
-        unique_list.append(category)
-        category_count[category] = 1 
-    else: 
-        category_count[category] += 1 
-
-for category, count in category_count.items():
-    print(category, count)
+#2. c)
+def unique_count(file_name, header):
+ """Displays the unique values along with how many rows they appear in, based on
+file_name
+ and header"""
+ unique_list = []
+ row_count = []
+ file_infile = open(file_name, "r") as file_infile:
+ csv_reader = csv.DictReader(file_infile)
+ for lst_cols in csv_reader:
+ value = lst_cols[header]
+ if value not in unique_list:
+ unique_list.append(value)
+ row_count.append(1)
+ else:
+ index = unique_list.index(value)
+ row_count[index] += 1
+ for i in range(len(unique_list)
+ file_infile.close()
